d4e053b9e0a357c3f855d83418480a52d2445a68
kalum.ost <kalum.ost@polymtl.ca>
2024-12-19 10:38:38 -0500

    [Minor] Added explicit (albeit redundant) label to clinical configs to prevent a dangling underscore

--- datasets/_utils/regen_configs.sh
+++ datasets/_utils/regen_configs.sh
@@ -1,5 +1,5 @@
 # Generate for the clinical-only dataset
-python generate_data_configs.py -s "../clinical/participants_cleaned.tsv" -o "../clinical/configs" -l "" --has_clinical
+python generate_data_configs.py -s "../clinical/participants_cleaned.tsv" -o "../clinical/configs" -l "clinical" --has_clinical
 
 # Generate for the DeepSeg (Binary) Datasets
 python generate_data_configs.py -s "../deepseg_binary/c2c6/full_*.tsv" -o "../deepseg_binary/c2c6/configs" -l "deepseg_binary_c2c6_binary" --has_clinical --has_imaging
