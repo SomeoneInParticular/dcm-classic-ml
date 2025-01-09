f868290a188174df890d35c96f40210665d02e5b
Kalum Ost <kalum.ost@polymtl.ca>
2025-01-09 01:15:31 -0500

    Updated config regeneration script to match the new generalized file structure

--- datasets/_utils/regen_configs.sh
+++ datasets/_utils/regen_configs.sh
@@ -1,14 +1,14 @@
 # Generate for the clinical-only dataset
-python generate_data_configs.py -s "../clinical/participants_cleaned.tsv" -o "../clinical/configs" -l "clinical" --has_clinical
+python generate_data_configs.py -s "../cleaned/clinical_only.tsv" -o "../cleaned/configs/clinical" -l "" --has_clinical
 
 # Generate for the DeepSeg (Binary) Datasets
-python generate_data_configs.py -s "../deepseg_binary/c2c6/full_*.tsv" -o "../deepseg_binary/c2c6/configs" -l "deepseg_binary_c2c6_binary" --has_clinical --has_imaging
-python generate_data_configs.py -s "../deepseg_binary/c2c7/full_*.tsv" -o "../deepseg_binary/c2c7/configs" -l "deepseg_binary_c2c7_binary" --has_clinical --has_imaging
-python generate_data_configs.py -s "../deepseg_binary/c2c6/img_*.tsv" -o "../deepseg_binary/c2c6/configs" -l "deepseg_binary_c2c6_binary" --has_imaging
-python generate_data_configs.py -s "../deepseg_binary/c2c7/img_*.tsv" -o "../deepseg_binary/c2c7/configs" -l "deepseg_binary_c2c7_binary" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/vert*.tsv" -o "../cleaned/configs/deepseg_binary_c2c6" -l "" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/vert*.tsv" -o "../cleaned/configs/deepseg_binary_c2c7" -l "" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/full*.tsv" -o "../cleaned/configs/deepseg_binary_c2c6" -l "" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/full*.tsv" -o "../cleaned/configs/deepseg_binary_c2c7" -l "" --has_imaging --has_clinical
 
 # Generate for the DeepSeg (Soft) Datasets
-python generate_data_configs.py -s "../deepseg_soft/c2c6/full_*.tsv" -o "../deepseg_soft/c2c6/configs" -l "deepseg_soft_c2c6_binary" --has_clinical --has_imaging
-python generate_data_configs.py -s "../deepseg_soft/c2c7/full_*.tsv" -o "../deepseg_soft/c2c7/configs" -l "deepseg_soft_c2c7_binary" --has_clinical --has_imaging
-python generate_data_configs.py -s "../deepseg_soft/c2c6/img_*.tsv" -o "../deepseg_soft/c2c6/configs" -l "deepseg_soft_c2c6_binary" --has_imaging
-python generate_data_configs.py -s "../deepseg_soft/c2c7/img_*.tsv" -o "../deepseg_soft/c2c7/configs" -l "deepseg_soft_c2c7_binary" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/vert*.tsv" -o "../cleaned/configs/deepseg_soft_c2c6" -l "" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/vert*.tsv" -o "../cleaned/configs/deepseg_soft_c2c7" -l "" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/full*.tsv" -o "../cleaned/configs/deepseg_soft_c2c6" -l "" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/full*.tsv" -o "../cleaned/configs/deepseg_soft_c2c7" -l "" --has_imaging --has_clinical
