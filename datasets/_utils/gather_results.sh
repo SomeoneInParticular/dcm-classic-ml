7edaa1c40236b0cc7319c931cca4084d2df0586a
kalum.ost <kalum.ost@polymtl.ca>
2025-01-10 13:53:24 -0500

    Added data gathering for the per-slice analyses

--- datasets/_utils/gather_results.sh
+++ datasets/_utils/gather_results.sh
@@ -6,3 +6,8 @@ cat /work/cadotte_lab/bids_cmri/derivatives/sct_6.5/*/*/deepsegv2_vertebrae_metr
 cat /work/cadotte_lab/bids_cmri/derivatives/sct_6.5/*/*/deepsegv2_vertebrae_metrics_c2c7.csv > "../raw/deepseg_binary_c2c7/vertebrae.csv"
 cat /work/cadotte_lab/bids_cmri/derivatives/sct_6.5/*/*/softseg_vertebrae_metrics.csv > "../raw/deepseg_soft_c2c6/vertebrae.csv"
 cat /work/cadotte_lab/bids_cmri/derivatives/sct_6.5/*/*/softseg_vertebrae_metrics_c2c7.csv > "../raw/deepseg_soft_c2c7/vertebrae.csv"
+
+cat /work/cadotte_lab/bids_cmri/derivatives/sct_6.5/*/*/deepsegv2_slice_metrics.csv > "../raw/deepseg_binary_c2c6/perslice.csv"
+cat /work/cadotte_lab/bids_cmri/derivatives/sct_6.5/*/*/deepsegv2_slice_metrics_c2c7.csv > "../raw/deepseg_binary_c2c7/perslice.csv"
+cat /work/cadotte_lab/bids_cmri/derivatives/sct_6.5/*/*/softseg_slice_metrics.csv > "../raw/deepseg_soft_c2c6/perslice.csv"
+cat /work/cadotte_lab/bids_cmri/derivatives/sct_6.5/*/*/softseg_slice_metrics_c2c7.csv > "../raw/deepseg_soft_c2c7/perslice.csv"
