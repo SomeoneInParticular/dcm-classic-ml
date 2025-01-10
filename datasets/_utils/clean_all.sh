05afa34de17da2a71b17efbccd7f010df5f6803b
kalum.ost <kalum.ost@polymtl.ca>
2025-01-10 14:02:28 -0500

    Updated clean-all to included per-slice data cleanup as well

--- datasets/_utils/clean_all.sh
+++ datasets/_utils/clean_all.sh
@@ -4,17 +4,35 @@ python clinical_cleanup.py
 # Then clean up each of the vertebral results
 python vertebral_cleanup.py \
   -i ../raw/deepseg_binary_c2c6/vertebral.csv \
-  -o ../cleaned/deepseg_binary_c2c6/ \
+  -o ../cleaned/deepseg_binary_c2c6/vertebral/ \
   --label 'vert_only'
 python vertebral_cleanup.py \
   -i ../raw/deepseg_binary_c2c7/vertebral.csv \
-  -o ../cleaned/deepseg_binary_c2c7/ \
+  -o ../cleaned/deepseg_binary_c2c7/vertebral/ \
   --label 'vert_only'
 python vertebral_cleanup.py \
   -i ../raw/deepseg_soft_c2c6/vertebral.csv \
-  -o ../cleaned/deepseg_soft_c2c6/ \
+  -o ../cleaned/deepseg_soft_c2c6/vertebral/ \
   --label 'vert_only'
 python vertebral_cleanup.py \
   -i ../raw/deepseg_soft_c2c7/vertebral.csv \
-  -o ../cleaned/deepseg_soft_c2c7/ \
+  -o ../cleaned/deepseg_soft_c2c7/vertebral/ \
+  --label 'vert_only'
+
+# And then clean up each of the per-slice results
+python perslice_cleanup.py \
+  -i ../raw/deepseg_binary_c2c6/perslice.csv \
+  -o ../cleaned/deepseg_binary_c2c6/perslice/ \
+  --label 'vert_only'
+python perslice_cleanup.py \
+  -i ../raw/deepseg_binary_c2c7/perslice.csv \
+  -o ../cleaned/deepseg_binary_c2c7/perslice/ \
+  --label 'vert_only'
+python perslice_cleanup.py \
+  -i ../raw/deepseg_soft_c2c6/perslice.csv \
+  -o ../cleaned/deepseg_soft_c2c6/perslice/ \
+  --label 'vert_only'
+python perslice_cleanup.py \
+  -i ../raw/deepseg_soft_c2c7/perslice.csv \
+  -o ../cleaned/deepseg_soft_c2c7/perslice/ \
   --label 'vert_only'
