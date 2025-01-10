11d4459b879d52d62d01cedbcc9b23dd8e46b110
kalum.ost <kalum.ost@polymtl.ca>
2025-01-10 14:24:58 -0500

    Added auto-script for generating the full datasets using the per-slice metrics

---
+++ datasets/_utils/generate_full_perslice_datasets.sh
@@ -0,0 +1,12 @@
+# For each image-derived dataset...
+for f in ../cleaned/*/perslice/slice_only*.tsv; do
+  # Determine the root path and label for the file
+  root_path=${f%*/*}
+  label=${f#*slice_only*}
+
+  # Identify the output path for the result
+  full_out="$root_path/full$label"
+
+  # Run the joining script to join this file with clinical metrics from "clinical_only.tsv"
+  python join_by_grp.py -i1 "../cleaned/clinical_only.tsv" -i2 $f -o $full_out
+done
