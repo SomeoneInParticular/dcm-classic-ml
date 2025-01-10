8d7818d918db948ccc5bfba76480af7a1da4d37f
kalum.ost <kalum.ost@polymtl.ca>
2025-01-10 14:05:00 -0500

    Added echoed headers to be displayed on the command line prior to running a cleaning script, to make debugging a bit easier.

--- datasets/_utils/clean_all.sh
+++ datasets/_utils/clean_all.sh
@@ -1,37 +1,46 @@
 # Clean up the clinical data first
+echo "## Clinical ##"
 python clinical_cleanup.py
 
 # Then clean up each of the vertebral results
+echo "## DeepSeg Binary C2C6 - Vertebral ##"
 python vertebral_cleanup.py \
   -i ../raw/deepseg_binary_c2c6/vertebral.csv \
   -o ../cleaned/deepseg_binary_c2c6/vertebral/ \
   --label 'vert_only'
+echo "## DeepSeg Binary C2C7 - Vertebral ##"
 python vertebral_cleanup.py \
   -i ../raw/deepseg_binary_c2c7/vertebral.csv \
   -o ../cleaned/deepseg_binary_c2c7/vertebral/ \
   --label 'vert_only'
+echo "## DeepSeg Soft C2C6 - Vertebral ##"
 python vertebral_cleanup.py \
   -i ../raw/deepseg_soft_c2c6/vertebral.csv \
   -o ../cleaned/deepseg_soft_c2c6/vertebral/ \
   --label 'vert_only'
+echo "## DeepSeg Soft C2C7 - Vertebral ##"
 python vertebral_cleanup.py \
   -i ../raw/deepseg_soft_c2c7/vertebral.csv \
   -o ../cleaned/deepseg_soft_c2c7/vertebral/ \
   --label 'vert_only'
 
 # And then clean up each of the per-slice results
+echo "## DeepSeg Binary C2C6 - Per-Slice ##"
 python perslice_cleanup.py \
   -i ../raw/deepseg_binary_c2c6/perslice.csv \
   -o ../cleaned/deepseg_binary_c2c6/perslice/ \
   --label 'vert_only'
+echo "## DeepSeg Binary C2C7 - Per-Slice ##"
 python perslice_cleanup.py \
   -i ../raw/deepseg_binary_c2c7/perslice.csv \
   -o ../cleaned/deepseg_binary_c2c7/perslice/ \
   --label 'vert_only'
+echo "## DeepSeg Soft C2C6 - Per-Slice ##"
 python perslice_cleanup.py \
   -i ../raw/deepseg_soft_c2c6/perslice.csv \
   -o ../cleaned/deepseg_soft_c2c6/perslice/ \
   --label 'vert_only'
+echo "## DeepSeg Soft C2C7 - Per-Slice ##"
 python perslice_cleanup.py \
   -i ../raw/deepseg_soft_c2c7/perslice.csv \
   -o ../cleaned/deepseg_soft_c2c7/perslice/ \
