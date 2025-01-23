f684cfe2a9e530de01dd2e1a695c41aa86ceeb59
kalum.ost <kalum.ost@polymtl.ca>
2025-01-23 15:14:44 -0500

    Fixed multiple different analyses trying (and often failing) to dump its results to the same output table (overwriting them in many cases)

--- datasets/_utils/regen_configs.sh
+++ datasets/_utils/regen_configs.sh
@@ -3,25 +3,25 @@ python generate_data_configs.py -s "../cleaned/clinical_only.tsv" -o "../configs
 
 # Generate for the DeepSeg (Binary) Datasets
 ## Vertebral-Metrics
-python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/vertebral/vert*.tsv" -o "../configs/deepseg_binary_c2c6/vertebral" -l "" --has_imaging
-python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/vertebral/vert*.tsv" -o "../configs/deepseg_binary_c2c7/vertebral" -l "" --has_imaging
-python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/vertebral/full*.tsv" -o "../configs/deepseg_binary_c2c6/vertebral" -l "" --has_imaging --has_clinical
-python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/vertebral/full*.tsv" -o "../configs/deepseg_binary_c2c7/vertebral" -l "" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/vertebral/vert*.tsv" -o "../configs/deepseg_binary_c2c6/vertebral" -l "binary_vertebral" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/vertebral/vert*.tsv" -o "../configs/deepseg_binary_c2c7/vertebral" -l "binary_vertebral" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/vertebral/full*.tsv" -o "../configs/deepseg_binary_c2c6/vertebral" -l "binary_vertebral" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/vertebral/full*.tsv" -o "../configs/deepseg_binary_c2c7/vertebral" -l "binary_vertebral" --has_imaging --has_clinical
 ## Per-slice Statistics
-python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/perslice/slice*.tsv" -o "../configs/deepseg_binary_c2c6/perslice" -l "" --has_imaging
-python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/perslice/slice*.tsv" -o "../configs/deepseg_binary_c2c7/perslice" -l "" --has_imaging
-python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/perslice/full*.tsv" -o "../configs/deepseg_binary_c2c6/perslice" -l "" --has_imaging --has_clinical
-python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/perslice/full*.tsv" -o "../configs/deepseg_binary_c2c7/perslice" -l "" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/perslice/slice*.tsv" -o "../configs/deepseg_binary_c2c6/perslice" -l "binary_perslice" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/perslice/slice*.tsv" -o "../configs/deepseg_binary_c2c7/perslice" -l "binary_perslice" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c6/perslice/full*.tsv" -o "../configs/deepseg_binary_c2c6/perslice" -l "binary_perslice" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_binary_c2c7/perslice/full*.tsv" -o "../configs/deepseg_binary_c2c7/perslice" -l "binary_perslice" --has_imaging --has_clinical
 
 
 # Generate for the DeepSeg (Soft) Datasets
 ## Vertebral-Metrics
-python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/vertebral/vert*.tsv" -o "../configs/deepseg_soft_c2c6/vertebral" -l "" --has_imaging
-python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/vertebral/vert*.tsv" -o "../configs/deepseg_soft_c2c7/vertebral" -l "" --has_imaging
-python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/vertebral/full*.tsv" -o "../configs/deepseg_soft_c2c6/vertebral" -l "" --has_imaging --has_clinical
-python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/vertebral/full*.tsv" -o "../configs/deepseg_soft_c2c7/vertebral" -l "" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/vertebral/vert*.tsv" -o "../configs/deepseg_soft_c2c6/vertebral" -l "soft_vertebral" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/vertebral/vert*.tsv" -o "../configs/deepseg_soft_c2c7/vertebral" -l "soft_vertebral" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/vertebral/full*.tsv" -o "../configs/deepseg_soft_c2c6/vertebral" -l "soft_vertebral" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/vertebral/full*.tsv" -o "../configs/deepseg_soft_c2c7/vertebral" -l "soft_vertebral" --has_imaging --has_clinical
 ## Per-slice Statistics
-python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/perslice/slice*.tsv" -o "../configs/deepseg_soft_c2c6/perslice" -l "" --has_imaging
-python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/perslice/slice*.tsv" -o "../configs/deepseg_soft_c2c7/perslice" -l "" --has_imaging
-python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/perslice/full*.tsv" -o "../configs/deepseg_soft_c2c6/perslice" -l "" --has_imaging --has_clinical
-python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/perslice/full*.tsv" -o "../configs/deepseg_soft_c2c7/perslice" -l "" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/perslice/slice*.tsv" -o "../configs/deepseg_soft_c2c6/perslice" -l "soft_perslice" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/perslice/slice*.tsv" -o "../configs/deepseg_soft_c2c7/perslice" -l "soft_perslice" --has_imaging
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c6/perslice/full*.tsv" -o "../configs/deepseg_soft_c2c6/perslice" -l "soft_perslice" --has_imaging --has_clinical
+python generate_data_configs.py -s "../cleaned/deepseg_soft_c2c7/perslice/full*.tsv" -o "../configs/deepseg_soft_c2c7/perslice" -l "soft_perslice" --has_imaging --has_clinical
